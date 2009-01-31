64341135a6935222a245c2ba206f2bcd76299dde
Bruce Williams <bruce@codefluency.com>
2009-01-30 21:06:32 -0600

    Move to canonical path Tweak docs Add sinatra recipe first so it ends up filling the default box Increment version for gem

--- lib/fiveruns/dash/sinatra.rb
+++ lib/fiveruns/dash/sinatra.rb
@@ -1,33 +1,23 @@
-gem 'fiveruns-dash-ruby'
 require 'fiveruns/dash'
 
+Fiveruns::Dash.logger.level = Logger::INFO
+
 Fiveruns::Dash.register_recipe :sinatra, :url => 'http://dash.fiveruns.com' do |recipe|
-  
   # NOTE: Captures static requests in addition to Sinatra app requests. Can't
   #       find a good way around that.
   recipe.counter :requests, :incremented_by => 'Sinatra::Application#dispatch!'
-  
   recipe.time :response_time, :method => 'Sinatra::Application#dispatch!'
-  
   recipe.time :render_time, :method => 'Sinatra::Application#render'
 end
 
-module Fiveruns
+module Fiveruns::Dash::Sinatra
   
-  module Dash
-    
-    module Sinatra
-      
-      def self.start(token)
-        return if Fiveruns::Dash.session.reporter.started?
-        Fiveruns::Dash.start(:app => token) do |config|
-          config.add_recipe :ruby
-          config.add_recipe :sinatra
-        end
-      end
-      
+  def self.start(token)
+    return if Fiveruns::Dash.session.reporter.started?
+    Fiveruns::Dash.start(:app => token) do |config|
+      config.add_recipe :sinatra
+      config.add_recipe :ruby
     end
-    
   end
   
 end
